3edd49086c855bbb1ea05dde1e78fa166d04814d
coutinhoigor <81998304+coutinhoigor@users.noreply.github.com>
2021-09-25 21:49:37 -0300

    Initial Commit

---
+++ BMI/BMIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMIApp.swift
+//  BMI
+//
+//  Created by Igor Coutinho Ferreira Nobre on 25/09/21.
+//
+
+import SwiftUI
+
+@main
+struct BMIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
